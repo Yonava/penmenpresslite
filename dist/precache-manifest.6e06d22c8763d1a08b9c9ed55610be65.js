d809b204935f9fa608ceac89b701826de6b09aa1
Yona Alexander Voss-Andreae <76519301+Yonava@users.noreply.github.com>
2021-11-25 00:29:10 -0800

    v4.1

---
+++ dist/precache-manifest.6e06d22c8763d1a08b9c9ed55610be65.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "766bf54b79d36d190750",
+    "url": "/css/app.025bb4e1.css"
+  },
+  {
+    "revision": "558ea5185f96d11684d3747cd7248f03",
+    "url": "/img/Dining-Hall-Disaster.558ea518.webp"
+  },
+  {
+    "revision": "35e48f6a840a556bc4929e53f8982e46",
+    "url": "/img/artgallery.35e48f6a.webp"
+  },
+  {
+    "revision": "cc36378510f920ab8aeb7e90c531d8fb",
+    "url": "/img/babykeem.cc363785.webp"
+  },
+  {
+    "revision": "a14514767f0ce96a229331da2e90d17b",
+    "url": "/img/combatingboredom.a1451476.webp"
+  },
+  {
+    "revision": "91f9c4b3376901975a961408c48bc890",
+    "url": "/img/construction.91f9c4b3.webp"
+  },
+  {
+    "revision": "cb70ccffe9166192886346cad7c31db7",
+    "url": "/img/homebutton.cb70ccff.png"
+  },
+  {
+    "revision": "12df04505c532cf6999c39e9bf71df31",
+    "url": "/img/radiosnhu.12df0450.webp"
+  },
+  {
+    "revision": "a50dddbdec28ef22faec5b8d40141e88",
+    "url": "/img/search.a50dddbd.png"
+  },
+  {
+    "revision": "aa0568fec9c56ed66c427d155388aff6",
+    "url": "/img/steak.aa0568fe.webp"
+  },
+  {
+    "revision": "51362ae4270ddbcd17f70fe06e5c0a59",
+    "url": "/img/timemanagement.51362ae4.webp"
+  },
+  {
+    "revision": "15dc84741b76cec3103649f039e0bd1b",
+    "url": "/img/tswift.15dc8474.webp"
+  },
+  {
+    "revision": "5ecb8978f88a08cf6a3bf8905c445e2b",
+    "url": "/img/wellnesscenter.5ecb8978.webp"
+  },
+  {
+    "revision": "3263d6567809a9d1072e104df8ef272e",
+    "url": "/img/wolak.3263d656.webp"
+  },
+  {
+    "revision": "98880f1652615461a600ba5f0ef17893",
+    "url": "/index.html"
+  },
+  {
+    "revision": "766bf54b79d36d190750",
+    "url": "/js/app.d5b08f00.js"
+  },
+  {
+    "revision": "5064eb48c43555a04aad",
+    "url": "/js/chunk-vendors.bd09a4d8.js"
+  },
+  {
+    "revision": "b86ea39119b5b4980581c48d0f79bb27",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
