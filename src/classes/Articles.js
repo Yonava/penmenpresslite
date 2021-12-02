b5c44b50ef243fc1cbec469a5d28489c4510775c
Yona Voss-Andreae <yonavossandreae@gmail.com>
2021-12-02 14:56:55 -0500

    v6.1

---
+++ src/classes/Articles.js
@@ -0,0 +1,12 @@
+export default class Articles {
+    constructor(title, image, imageCaption, category, author, date, content) {
+        this.title = title;
+        this.image = image;
+        this.imageCaption = imageCaption;
+        this.category = category;
+        this.author = author;
+        this.date = date;
+        this.content = content;
+        this.saved = false;
+    }
+}
