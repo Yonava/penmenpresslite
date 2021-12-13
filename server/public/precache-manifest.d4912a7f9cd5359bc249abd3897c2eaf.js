b8c732a6bce0823668992a45958b24de8c78e7cf
Yona Voss-Andreae <yonavossandreae@gmail.com>
2021-12-13 17:30:27 -0500

    v8.31

---
+++ server/public/precache-manifest.d4912a7f9cd5359bc249abd3897c2eaf.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8903821f7ddd13757b83",
+    "url": "/css/app.6a3bb04d.css"
+  },
+  {
+    "revision": "5c367b3ea51dba3c4367aa8bdcbbf098",
+    "url": "/img/add-bookmark.5c367b3e.svg"
+  },
+  {
+    "revision": "c48fd12eec18a03e75d2155d4d4a70db",
+    "url": "/img/backsearch.c48fd12e.svg"
+  },
+  {
+    "revision": "2e5b6d2691b2dd6f43f4170a165ff48a",
+    "url": "/img/discovericon.2e5b6d26.svg"
+  },
+  {
+    "revision": "78d6179dc2a42b9c4f6411059533e186",
+    "url": "/img/facebook.78d6179d.svg"
+  },
+  {
+    "revision": "f27ffd8aa50b7a241baa3fe75f0876f8",
+    "url": "/img/feedicon.f27ffd8a.svg"
+  },
+  {
+    "revision": "4cb5424240d4916086dc1b717f5ed592",
+    "url": "/img/font.4cb54242.svg"
+  },
+  {
+    "revision": "6e01c9d79adb98f7a6efa242e33afc4a",
+    "url": "/img/instagram.6e01c9d7.svg"
+  },
+  {
+    "revision": "04f00413ba9f7a6df757e6a69bd6c200",
+    "url": "/img/refresh.04f00413.svg"
+  },
+  {
+    "revision": "26554516547f4a3a240220633d172fd7",
+    "url": "/img/remove-bookmark.26554516.svg"
+  },
+  {
+    "revision": "b8ff736b7875479e2d79d4a6c1a2344c",
+    "url": "/img/savedicon-filled.b8ff736b.svg"
+  },
+  {
+    "revision": "b83b1fc0ab0f952959764b85e57c772a",
+    "url": "/img/savedicon.b83b1fc0.svg"
+  },
+  {
+    "revision": "acf08b29fd128fb0616f84d4c7d9cf92",
+    "url": "/img/search.acf08b29.svg"
+  },
+  {
+    "revision": "9d02641e74afc1cd6a7047da7552cba5",
+    "url": "/img/share.9d02641e.svg"
+  },
+  {
+    "revision": "b107fca272d4ecc126d9526cba95f7a8",
+    "url": "/img/top-button.b107fca2.svg"
+  },
+  {
+    "revision": "ea3d460248676823325521508997931d",
+    "url": "/img/trendingicon.ea3d4602.svg"
+  },
+  {
+    "revision": "490d041f3025837f4fb6d2af9d5e9839",
+    "url": "/img/website.490d041f.svg"
+  },
+  {
+    "revision": "7a7927d6395e6a9feeb2f32ec3aa6d88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8903821f7ddd13757b83",
+    "url": "/js/app.12048d55.js"
+  },
+  {
+    "revision": "564a700a2e53f5758e26",
+    "url": "/js/chunk-vendors.3f471d99.js"
+  },
+  {
+    "revision": "24768a514b66a141e639c6fd7c7cb15a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "7eeeb01f9a5e1c568b630f21bee55229",
+    "url": "/splashscreens/ipad_splash.png"
+  },
+  {
+    "revision": "345265fba5f9dc18b6e06d545997e6a7",
+    "url": "/splashscreens/ipadpro1_splash.png"
+  },
+  {
+    "revision": "e955364ab2da0baab668c1619b4ae140",
+    "url": "/splashscreens/ipadpro2_splash.png"
+  },
+  {
+    "revision": "42dcd1451842778f93a187afd2177aa7",
+    "url": "/splashscreens/ipadpro3_splash.png"
+  },
+  {
+    "revision": "409ed827509bbdca0739e822201a5c17",
+    "url": "/splashscreens/iphone5_splash.png"
+  },
+  {
+    "revision": "113a4b839fe9f6152ea58c84cab335b7",
+    "url": "/splashscreens/iphone6_splash.png"
+  },
+  {
+    "revision": "86e446798fc6e997f81f4d617c49363f",
+    "url": "/splashscreens/iphoneplus_splash.png"
+  },
+  {
+    "revision": "253898f505f2bd170aae1a14bc96f317",
+    "url": "/splashscreens/iphonex_splash.png"
+  },
+  {
+    "revision": "47345a03191668bc9a514dea2559aa82",
+    "url": "/splashscreens/iphonexr_splash.png"
+  },
+  {
+    "revision": "ae24ece5e8c65672949492af06b60284",
+    "url": "/splashscreens/iphonexsmax_splash.png"
+  }
+]);
