dcad0f91c08487f8a58f43a6d6e6f05c86216f89
Yonava <yonavossandreae@gmail.com>
2022-12-02 01:26:59 -0500

    mysql aws rds hooked up

--- server/index.js
+++ server/index.js
@@ -1,27 +1,33 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+  host: 'press.c0w2juzukesp.us-east-1.rds.amazonaws.com',
+  port: '3306',
+  user: 'admin',
+  password: 'cs231db!'
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('Connected to database');
+});
 
 const app = express();
 
-// middleware
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 
 const articles = require('./routes/api/articles');
 app.use('/api/articles', articles);
 
-mongoose.connect("\mongodb+srv://press:dpM2qhDcvhsbwxOk@cluster0.wfzfa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", () => console.log('Successfully Connected'))
-
-// Handle production
 if (process.env.NODE_ENV === 'production') {
-    // Static folder
-    app.use(express.static(__dirname + '/public/'));
-  
-    // Handle SPA
-    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+  app.use(express.static(__dirname + '/public/'));
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 }
 
 const port = process.env.PORT || 5001;
--- server/routes/api/articles.js
+++ server/routes/api/articles.js
@@ -1,71 +1,30 @@
 const express = require('express');
-const Article = require('../../models/Articles');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
 
-    console.log("getting stuff")
-    try {
-        const articles = await Article.find();
-        res.json(articles);
-    } catch (error) {
-        res.json({ message: error });
-    }
+  
 });
 
 router.get('/:id', async (req, res) => {
 
-    try {
-        const articles = await Article.findOne({ _id: req.params.id });
-        res.json(articles);
-    } catch (error) {
-        res.json({ message: error });
-    }
+  
 })
 
 router.post('/', async (req, res) => {
 
-    const article = new Article({
-        title: req.body.title,
-        content: req.body.content,
-        image: req.body.image,
-        imageCaption: req.body.imageCaption,
-        category: req.body.category,
-        author: req.body.author,
-        date: req.body.date
-    });
-
-    try {
-        const savedArticle = await article.save();
-        res.json(savedArticle);
-    } catch (error) {
-        res.json({ message: error });
-    }
+  
 
 });
 
 router.delete('/:id', async (req, res) => {
 
-    try {
-        const removedArticle = await Article.deleteOne({ _id: req.params.id });
-        res.json(removedArticle);
-    } catch (error) {
-        res.json({ message: error })
-    }
+  
 });
 
 router.put('/:id', async (req, res) => {
 
-    try {
-        const updatedArticle = await Article.updateOne(
-            { _id: req.params.id }, 
-            { $set: 
-            { score: req.body.score } }
-        );
-        res.json(updatedArticle);
-    } catch (error) {
-        res.json({ message: error })
-    }
+  
 });
 
 module.exports = router;
